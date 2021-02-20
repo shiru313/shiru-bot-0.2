7de1d65961b49072f3961d3830206e9be9d3593f
shiru313 <72118394+shiru313@users.noreply.github.com>
2021-02-20 16:30:46 +0530

    Add files via upload

---
+++ plugins/system_stats.js
@@ -0,0 +1,33 @@
+/* Copyright (C) 2020 Yusuf Usta.
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+WhatsAsena - Yusuf Usta
+Developer & Co-Founder - Phaticusthiccy
+*/
+
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const {spawnSync} = require('child_process');
+const Config = require('../config');
+const chalk = require('chalk');
+
+const Language = require('../language');
+const Lang = Language.getString('system_stats');
+
+Asena.addCommand({pattern: 'alive', fromMe: true, desc: Lang.ALIVE_DESC}, (async (message, match) => {
+    if (Config.ALIVEMSG == 'default') {
+        await message.client.sendMessage(message.jid,  '  ```hy bro iam alive😊``` '  , MessageType.text);
+    }
+    else {
+        await message.client.sendMessage(message.jid,Config.ALIVEMSG + '\n*Powered by shiru bot*', MessageType.text);
+    }
+}));
+
+Asena.addCommand({pattern: 'sysd', fromMe: true, desc: Lang.SYSD_DESC}, (async (message, match) => {
+    const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
+    await message.sendMessage(
+        '```' + child + '```', MessageType.text
+    );
+}));
